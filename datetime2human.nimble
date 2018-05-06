9dff54386c9e11b12d099adf0125c338ad8c74c9
juancarlospaco <juancarlospaco@gmail.com>
2018-05-05 23:17:37 -0300

    Init

---
+++ datetime2human.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version       = "0.1.0"
+author        = "juancarlospaco"
+description   = "Human friendly DateTime string representations, seconds to millenniums."
+license       = "LGPLv3"
+srcDir        = "src"
+
+# Dependencies
+
+requires "nim >= 0.18.1"
+requires "pylib"
---
+++ src/datetime2human.nim
@@ -0,0 +1,116 @@
+#!/usr/bin/nim c -r
+
+
+import times
+import pylib
+import strformat
+import strutils
+
+
+type HumanFriendlyTimeUnits = tuple[
+  seconds: int, minutes: int, hours: int, days: int, weeks: int,
+  months: int, years: int, decades: int, centuries: int, millenniums: int]
+type HumanTimes = tuple[
+  human: string, short: string, iso: string, units: HumanFriendlyTimeUnits]
+
+
+proc datetime2human*(datetime_obj: DateTime, iso_sep: char=' '): HumanTimes =
+  ## Calculate date & time, with precision from seconds to millenniums.
+
+  var seconds: int
+  var minutes: int
+  var hours: int
+  var days: int
+  var weeks: int
+  var months: int
+  var years: int
+  var decades: int
+  var centuries: int
+  var millenniums: int
+
+  (minutes, seconds)       = divmod(int(toUnix(datetime_obj.toTime)), 60)
+  (hours, minutes)         = divmod(minutes, 60)
+  (days, hours)            = divmod(hours, 24)
+  (weeks, days)            = divmod(days, 7)
+  (months, weeks)          = divmod(weeks, 4)
+  (years, months)          = divmod(months, 12)
+  (decades, years)         = divmod(years, 10)
+  (centuries, decades)     = divmod(decades, 10)
+  (millenniums, centuries) = divmod(centuries, 10)
+
+  # Build a tuple with all named time units and all its integer values.
+  let time_units: HumanFriendlyTimeUnits = (
+      seconds: seconds, minutes: minutes, hours: hours, days: days,
+      weeks: weeks, months: months, years: years, decades: decades,
+      centuries: centuries, millenniums: millenniums)
+
+  # Build a human friendly time string with frequent time units.
+  var time_parts: seq[string]
+  var human_time_auto = ""
+  var this_time = ""
+  if millenniums:
+      this_time = fmt"{millenniums} Millenniums"
+      if not human_time_auto:
+          human_time_auto = this_time
+      time_parts.add(this_time)
+  if centuries:
+      this_time = fmt"{centuries} Centuries"
+      if not human_time_auto:
+          human_time_auto = this_time
+      time_parts.add(this_time)
+  if decades:
+      this_time = fmt"{decades} Decades"
+      if not human_time_auto:
+          human_time_auto = this_time
+      time_parts.add(this_time)
+  if years:
+      this_time = fmt"{years} Years"
+      if not human_time_auto:
+          human_time_auto = this_time
+      time_parts.add(this_time)
+  if months:
+      this_time = fmt"{months} Months"
+      if not human_time_auto:
+          human_time_auto = this_time
+      time_parts.add(this_time)
+  if weeks:
+      this_time = fmt"{weeks} Weeks"
+      if not human_time_auto:
+          human_time_auto = this_time
+      time_parts.add(this_time)
+  if days:
+      this_time = fmt"{days} Days"
+      if not human_time_auto:
+          human_time_auto = this_time
+      time_parts.add(this_time)
+  if hours:
+      this_time = fmt"{hours} Hours"
+      if not human_time_auto:
+          human_time_auto = this_time
+      time_parts.add(this_time)
+  if minutes:
+      this_time = fmt"{minutes} Minutes"
+      if not human_time_auto:
+          human_time_auto = this_time
+      time_parts.add(this_time)
+  if not human_time_auto:
+      human_time_auto = fmt"{seconds} Seconds"
+  time_parts.add(fmt"{seconds} Seconds")
+
+  # Get a Human string ISO-8601 representation.
+  let iso_datetime = str(datetime_obj).replace("T", $iso_sep)
+
+  let t: HumanTimes = (
+    human: " ".join(time_parts), short: human_time_auto,
+    iso: iso_datetime, units: time_units)
+  t
+
+
+proc now2human*(): HumanTimes =  # Just a shortcut :)
+  ## Now expressed as human friendly time units string.
+  datetime2human(now())
+
+
+if is_main_module:
+  echo datetime2human(now())
+  echo now2human()
