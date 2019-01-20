b0aa07ef74e29dca8ad515c5f7fcd845c5de85ee
juancarlospaco <juancarlospaco@gmail.com>
2019-01-20 03:41:05 -0300

    Remove dependency on PyLib

--- src/datetime2human.nim
+++ src/datetime2human.nim
@@ -8,18 +8,19 @@ type HumanFriendlyTimeUnits* = tuple[
 type HumanTimes* = tuple[
   human: string, short: string, iso: string, units: HumanFriendlyTimeUnits] ## Tuple of Human Friendly Time Units as strings.
 
+
 func divmod(a, b: SomeInteger): array[0..1, int] {.inline.} =
   [int(a / b), int(a mod b)]
 
-func datetime2human*(datetime_obj: DateTime, iso_sep: char=' ', lower=false): HumanTimes =
+func datetime2human*(datetimeObj: DateTime, isoSep: char=' ', lower=false): HumanTimes =
   ## Calculate date & time, with precision from seconds to millenniums.
 
   var
-    time_parts: seq[string]
-    human_time_auto, this_time: string
+    timeParts: seq[string]
+    humanTimeAuto, thisTime: string
     seconds, minutes, hours, days, weeks, months, years, decades, centuries, millenniums: int
 
-  (minutes, seconds)       = divmod(int(toUnix(datetime_obj.toTime)), 60)
+  (minutes, seconds)       = divmod(int(toUnix(datetimeObj.toTime)), 60)
   (hours, minutes)         = divmod(minutes, 60)
   (days, hours)            = divmod(hours, 24)
   (weeks, days)            = divmod(days, 7)
@@ -30,78 +31,78 @@ func datetime2human*(datetime_obj: DateTime, iso_sep: char=' ', lower=false): Hu
   (millenniums, centuries) = divmod(centuries, 10)
 
   # Build a tuple with all named time units and all its integer values.
-  let time_units: HumanFriendlyTimeUnits = (
+  let timeUnits: HumanFriendlyTimeUnits = (
     seconds: seconds, minutes: minutes, hours: hours, days: days,
     weeks: weeks, months: months, years: years, decades: decades,
     centuries: centuries, millenniums: millenniums)
 
   # Build a human friendly time string with frequent time units.
   if millenniums > 0:
-      this_time = $millenniums & " Millenniums"
-      if human_time_auto.len == 0:
-          human_time_auto = this_time
-      time_parts.add(this_time)
+      thisTime = $millenniums & " Millenniums"
+      if humanTimeAuto.len == 0:
+          humanTimeAuto = thisTime
+      timeParts.add(thisTime)
   if centuries > 0:
-      this_time = $centuries & " Centuries"
-      if human_time_auto.len == 0:
-          human_time_auto = this_time
-      time_parts.add(this_time)
+      thisTime = $centuries & " Centuries"
+      if humanTimeAuto.len == 0:
+          humanTimeAuto = thisTime
+      timeParts.add(thisTime)
   if decades > 0:
-      this_time = $decades & " Decades"
-      if human_time_auto.len == 0:
-          human_time_auto = this_time
-      time_parts.add(this_time)
+      thisTime = $decades & " Decades"
+      if humanTimeAuto.len == 0:
+          humanTimeAuto = thisTime
+      timeParts.add(thisTime)
   if years > 0:
-      this_time = $years & " Years"
-      if human_time_auto.len == 0:
-          human_time_auto = this_time
-      time_parts.add(this_time)
+      thisTime = $years & " Years"
+      if humanTimeAuto.len == 0:
+          humanTimeAuto = thisTime
+      timeParts.add(thisTime)
   if months > 0:
-      this_time = $months & " Months"
-      if human_time_auto.len == 0:
-          human_time_auto = this_time
-      time_parts.add(this_time)
+      thisTime = $months & " Months"
+      if humanTimeAuto.len == 0:
+          humanTimeAuto = thisTime
+      timeParts.add(thisTime)
   if weeks > 0:
-      this_time = $weeks & " Weeks"
-      if human_time_auto.len == 0:
-          human_time_auto = this_time
-      time_parts.add(this_time)
+      thisTime = $weeks & " Weeks"
+      if humanTimeAuto.len == 0:
+          humanTimeAuto = thisTime
+      timeParts.add(thisTime)
   if days > 0:
-      this_time = $days & " Days"
-      if human_time_auto.len == 0:
-          human_time_auto = this_time
-      time_parts.add(this_time)
+      thisTime = $days & " Days"
+      if humanTimeAuto.len == 0:
+          humanTimeAuto = thisTime
+      timeParts.add(thisTime)
   if hours > 0:
-      this_time = $hours & " Hours"
-      if human_time_auto.len == 0:
-          human_time_auto = this_time
-      time_parts.add(this_time)
+      thisTime = $hours & " Hours"
+      if humanTimeAuto.len == 0:
+          humanTimeAuto = thisTime
+      timeParts.add(thisTime)
   if minutes > 0:
-      this_time = $minutes & " Minutes"
-      if human_time_auto.len == 0:
-          human_time_auto = this_time
-      time_parts.add(this_time)
-  if human_time_auto.len == 0:
-      human_time_auto = $seconds & " Seconds"
-  time_parts.add($seconds & " Seconds")
+      thisTime = $minutes & " Minutes"
+      if humanTimeAuto.len == 0:
+          humanTimeAuto = thisTime
+      timeParts.add(thisTime)
+  if humanTimeAuto.len == 0:
+      humanTimeAuto = $seconds & " Seconds"
+  timeParts.add($seconds & " Seconds")
 
   # Get a Human string ISO-8601 representation.
-  let iso_datetime = replace($datetime_obj, "T", $iso_sep)
+  let isoDatetime = replace($datetimeObj, "T", $isoSep)
 
   result = (
-    human: if lower: time_parts.join(" ").toLowerAscii else: time_parts.join(" "),
-    short: if lower: human_time_auto.toLowerAscii else: human_time_auto,
-    iso: iso_datetime, units: time_units)
+    human: if lower: timeParts.join(" ").toLowerAscii else: timeParts.join(" "),
+    short: if lower: humanTimeAuto.toLowerAscii else: humanTimeAuto,
+    iso: isoDatetime, units: timeUnits)
 
 
-proc now2human*(iso_sep: char=' ', lower=false): HumanTimes {.inline.} =  # Just a shortcut :)
+proc now2human*(isoSep: char=' ', lower=false): HumanTimes {.inline.} =  # Just a shortcut :)
   ## Now expressed as human friendly time units string.
-  datetime2human(now(), iso_sep, lower)
+  datetime2human(now(), isoSep, lower)
 
 
 runnableExamples:
   import times
   echo datetime2human(now())
   echo now2human()
-  echo datetime2human(now(), iso_sep='X', lower=true)
-  echo now2human(iso_sep='X', lower=true)
+  echo datetime2human(now(), isoSep='X', lower=true)
+  echo now2human(isoSep='X', lower=true)
